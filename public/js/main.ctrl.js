f2d1426abbbc99cf0e86a65df2b491afb2c42e68
Dong Nguyen <ndaidong@gmail.com>
2015-04-07 13:45:42 +0700

    Update

--- public/js/main.ctrl.js
+++ public/js/main.ctrl.js
@@ -4,6 +4,8 @@
 
 ;(function(){
 
+    'use strict';
+
     angular
     .module('app')
     .controller('MainController', MainController);
@@ -16,10 +18,33 @@
 
         vm.players = [];
 
-        $http.get('https://ng-api-ndaidong.c9.io/').success(function(data){
-            console.log(data);
-            vm.players = data.result.entries;
-        });
+        // pagination
+        vm.everyPage = 30;
+        vm.currentPage = 1;
+        vm.totalPlayer = 0;
+
+        vm.loadPage = function(p){
+
+            var ps = vm.everyPage;
+            var start = (p-1)*ps;
+            var end = start+ps;
+
+            var url = 'http://ng-api.techpush.net/players';
+                url+='?_start='+start;
+                url+='&_end='+end;
+
+            $http.get(url).success(function(data){
+                console.log(data);
+                if(!!data && !data.error){
+                    var result = data.result;
+                    vm.totalPlayer = result.totalItems;
+                    vm.players = result.entries;
+                    vm.currentPage = p;
+                }
+            });
+        }
+
+        vm.loadPage(1);
     }
 
 })();
